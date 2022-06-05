efffe57ea73c5f0f9e373b43236bde91a9806c27
Alexandre <pedestaljunior@gmail.com>
2022-06-05 20:55:10 -0300

    correção console

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -16,36 +16,30 @@ const data = loadLists();
 
 export default function Board() {
   const [lists, setLists] = useState(data);
-  const [isMovingCard, setIsMovingCard] = useState(false);
 
 
   const handleDrop = useCallback(
     (index, item) => {
-      if(!isMovingCard){
-        setLists(produce(lists, draft => {
-          const dragged = draft[item.listIndex].cards[item.index];
-          draft[item.listIndex].cards.splice(item.index, 1);
-          draft[index].cards.push(dragged);
-        }))
-      }else{
-        setIsMovingCard(false);
-      }
+      setLists(produce(lists, draft => {
+        const dragged = draft[item.listIndex].cards[item.index];
+        draft[item.listIndex].cards.splice(item.index, 1);
+        draft[index].cards.push(dragged);
+      }))
+      
     },
+    [lists]
   )
 
-  function move(fromList, toList, from, to) {
-    setLists(produce(lists, draft => {
-      const dragged = draft[fromList].cards[from];
-      setIsMovingCard(true);
-      draft[fromList].cards.splice(from, 1);
-      draft[toList].cards.splice(to, 0, dragged);
-    }))
-  }
-
   return (
-    <BoardContext.Provider value={{ lists, move }}>
+    <BoardContext.Provider value={{ lists }}>
       <Container>
-        {lists.map((list, index) => <List accepts={['CARD']} onDrop={item => handleDrop(index, item)} key={list.title} index={index} data={list} />)}
+        {lists.map((list, index) => 
+          <List accepts={['CARD']} 
+                onDrop={item => handleDrop(index, item)} 
+                key={list.title} 
+                index={index} 
+                data={list} 
+          />)}
       </Container>
     </BoardContext.Provider>
   );
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,10 @@
-import React, { useRef, useContext } from 'react';
+import React, { useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
-
-import BoardContext from '../Board/context';
 
 import { Container, Label } from './styles';
 
 export default function Card({ data, index, listIndex }) {
   const ref = useRef();
-  const { move } = useContext(BoardContext);
-  let memTime = 0;
   const [{ isDragging }, dragRef] = useDrag({
     item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
@@ -17,40 +13,7 @@ export default function Card({ data, index, listIndex }) {
   });
 
   const [, dropRef] = useDrop({
-    accept: 'CARD',
-    hover(item, monitor) {
-      var date = new Date();
-      var now = date.getTime();
-      if(memTime == 0) memTime = now;
-      const draggedListIndex = item.listIndex;
-      const targetListIndex = listIndex;
-
-      const draggedIndex = item.index;
-      const targetIndex = index;
-
-      if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
-        return;
-      }
-
-      const targetSize = ref.current.getBoundingClientRect();
-      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
-
-      const draggedOffset = monitor.getClientOffset();
-      const draggedTop = draggedOffset.y - targetSize.top;
-
-      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
-        return;
-      }
-      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
-        return;
-      }
-      if((now - memTime) >= 300 || draggedListIndex == targetListIndex){
-        move(draggedListIndex, targetListIndex, draggedIndex, targetIndex,'card');
-        item.index = targetIndex;
-        item.listIndex = targetListIndex;
-        memTime = 0;
-      }
-    }
+    accept: 'CARD'
   })
 
   dragRef(dropRef(ref));
